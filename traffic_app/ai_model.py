c1a1407a8bae23e073fbd9877274e01d14e22236
raghur-relangi <raghuramanarasimharelangi102@gmail.com>
2026-05-01 08:42:41 +0530

    Fix OpenCV display issue for server

--- traffic_app/ai_model.py
+++ traffic_app/ai_model.py
@@ -1,7 +1,13 @@
+import os
+os.environ["DISPLAY"] = ""
+os.environ["OPENCV_VIDEOIO_PRIORITY_BACKEND"] = "0"
+os.environ["QT_QPA_PLATFORM"] = "offscreen"
+cv2.setNumThreads(0)
+
 import cv2
 
 def detect_traffic(video_path):
-    cap = cv2.VideoCapture(video_path)
+    cap = cv2.VideoCapture(video_path, cv2.CAP_FFMPEG)
 
     vehicle_count = 0
     frame_count = 0
@@ -14,13 +20,11 @@ def detect_traffic(video_path):
 
         frame_count += 1
 
-        # Simple logic (simulate detection)
         if frame_count % 30 == 0:
             vehicle_count += 1
 
     cap.release()
 
-    # Simple anomaly rule
     anomaly = True if vehicle_count > 20 else False
 
     return vehicle_count, anomaly
